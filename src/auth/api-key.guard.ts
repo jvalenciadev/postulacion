aa23742938ac7ed675a183f9f4848e7580611431
jvalenciadev <jpvalencia.developer@gmail.com>
2026-01-18 06:34:12 -0400

    mejoras

--- src/auth/api-key.guard.ts
+++ src/auth/api-key.guard.ts
@@ -7,7 +7,7 @@ export class ApiKeyGuard implements CanActivate {
 
   canActivate(context: ExecutionContext): boolean {
     const request = context.switchToHttp().getRequest();
-    const apiKey = request.headers['X-API-KEY'];
+    const apiKey = request.headers['x-api-key'];
     const validApiKey = this.configService.get<string>('API_KEY');
 
     if (!apiKey || apiKey !== validApiKey) {
